ad5b209fa76ecdbff367a731d658e44a210629dd
anastasiiavol <anastasiiavolchanska@gmail.com>
2020-02-29 01:06:42 +0200

    item page adaptive

---
+++ filter.js
@@ -0,0 +1,13 @@
+'use strict';
+
+
+let dropdownFilter = document.getElementsByClassName('dropdown-btn_content');
+
+for (let i = 0; i < dropdownFilter.length; i++) {
+    for (let j = 0; j < dropdownFilter[i].children.length; j++) {
+        dropdownFilter[i].children[j].addEventListener('click', function () {
+            this.parentNode.previousElementSibling.innerHTML = this.innerHTML;
+        })
+    }
+
+}
